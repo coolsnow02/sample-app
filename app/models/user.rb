6e0c121ad0283370c3974da46c4c20264f43fb94
nehagwebonise <webonise@webonise-desktop.(none)>
2012-09-21 15:20:22 +0530

    corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,8 @@ class User < ActiveRecord::Base
   validates :name, :presence => true, :length => { :maximum => 50 }, :uniqueness => true
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email, :presence => true, :format => { :with => email_regex}, :uniqueness => {case_sensitive => false}
+case_sensitive
+  validates :email, :presence => true, :format => { :with => email_regex}, :uniqueness => {:case_sensitive => false}
 
   validates :password, :presence => true, :confirmation => true, length => { :within => 6..40}
 
