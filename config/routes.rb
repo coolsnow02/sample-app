fc4a660814a7b814930a089d91f8adf41178259d
nehagwebonise <webonise@webonise-desktop.(none)>
2012-09-21 11:30:53 +0530

    modification in root html file name

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+
   get "pages/home"
 
   get "pages/contact"
@@ -6,7 +7,16 @@ SampleApp::Application.routes.draw do
   get "pages/about"
 
   get "pages/help"
-  # The priority is based upon order of creation:
+
+  match '/', :to => 'pages#home'
+
+  match '/contact', :to => 'pages#contact'
+
+    match '/about', :to => 'pages#about'
+
+    match '/help', :to => 'pages#help'
+
+    # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
@@ -55,7 +65,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+   root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
